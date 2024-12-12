9909746d5d048b6ff2b589ba51f15e5af8d8b692
korneliarhyu <korneliarhyu2@gmail.com>
2024-12-12 16:52:48 +0700

    fix form_detail

--- lib/view/form_detail.dart
+++ lib/view/form_detail.dart
@@ -11,7 +11,6 @@ class FormDetail extends StatefulWidget {
   _FormDetailState createState() => _FormDetailState();
 }
 
-
 class _FormDetailState extends State<FormDetail> {
   Map<String, dynamic>? _formData;
   String? _formName;
@@ -31,59 +30,83 @@ class _FormDetailState extends State<FormDetail> {
   Future<void> _loadFormData(int formId) async {
     try {
       var form = await DatabaseHelper.getFormById(formId);
-      //debug form data
-      //print("Form data: $form");
-
       if (form == null) {
-        setState(() {
-          _isLoading = false;
-        });
-        print("Form data not found!");
+        setState(() => _isLoading = false);
+        print("Form data not found!: $formId");
         return;
       }
 
-        var template = await DatabaseHelper.getTemplateById(form['templateId']);
+      print("Form data retrieved: $form");
 
-        if (template == null) {
-          setState(() {
-            _isLoading = false;
-          });
-          print("Template data not found!");
-          return;
-        }
-
-        //debug template data
-        //print("Template data: ${template['templateFormData']}");
-
-        Map<String, dynamic> templateData = jsonDecode(template['templateFormData']);
-        Map<String, dynamic> formData = form['formData'] != null
-            ? jsonDecode(form['formData'])
-            : {};
-
-        Map<String, dynamic> displayedForm = {};
-        templateData['questions'].forEach((key, question){
-          displayedForm[key] = {
-            ...question,
-            'answer': formData[key] ?? '',
-          };
-        });
-
-        setState(() {
-          // Decode the JSON stored in the formData column
-          _formName = form['formName'];
-          //debug merged form
-          //print("Merged Form: $displayedForm");
-          _formData = displayedForm;
-          _isLoading = false;
-        });
-
-      } catch (e) {
+      var formData =
+          form['formData'] != null ? jsonDecode(form['formData']) : {};
+      print("Decoded form data: $formData");
       setState(() {
+        _formName = form['formName'];
+        _formData = formData;
         _isLoading = false;
       });
-      print("Error loading form or template data: $e");
+    } catch (e) {
+      setState(() => _isLoading = false);
+      print("Error loading form data: $e");
     }
   }
+
+  // Future<void> _loadFormData(int formId) async {
+  //   try {
+  //     var form = await DatabaseHelper.getFormById(formId);
+  //     //debug form data
+  //     //print("Form data: $form");
+  //
+  //     if (form == null) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       print("Form data not found!");
+  //       return;
+  //     }
+  //
+  //     var template = await DatabaseHelper.getTemplateById(form['templateId']);
+  //
+  //     if (template == null) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       print("Template data not found!");
+  //       return;
+  //     }
+  //
+  //     //debug template data
+  //     //print("Template data: ${template['templateFormData']}");
+  //
+  //     Map<String, dynamic> templateData =
+  //         jsonDecode(template['templateFormData']);
+  //     Map<String, dynamic> formData =
+  //         form['formData'] != null ? jsonDecode(form['formData']) : {};
+  //
+  //     Map<String, dynamic> displayedForm = {};
+  //     templateData['questions'].forEach((key, question) {
+  //       displayedForm[key] = {
+  //         ...question,
+  //         'answer': formData[key] ?? '',
+  //       };
+  //     });
+  //
+  //     setState(() {
+  //       // Decode the JSON stored in the formData column
+  //       _formName = form['formName'];
+  //       //debug merged form
+  //       //print("Merged Form: $displayedForm");
+  //       _formData = displayedForm;
+  //       _isLoading = false;
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //     print("Error loading form or template data: $e");
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -119,45 +142,47 @@ class _FormDetailState extends State<FormDetail> {
                       ),
                     );
                   },
-              ),
+                ),
     );
   }
 
-  // Use your existing method to build questions
   List<Widget> _buildQuestions(Map<String, dynamic> questions) {
-    print("Building questions: $questions");
     List<Widget> questionWidgets = [];
+    print("Building questions from: $questions");
+
     questions.forEach((key, question) {
-      print("Processing: $key -> $question");
-      if (question is Map<String, dynamic>) {
-        switch (question['type']) {
-          case 'dropdown':
-            questionWidgets.add(_buildDropdownQuestion(key, question));
-            break;
-          case 'multiple':
-            questionWidgets.add(_buildMultipleChoiceQuestion(key, question));
-            break;
-          case 'text':
-            questionWidgets.add(_buildTextQuestion(key, question));
-            break;
-          default:
-            print("Unknown question type: ${question['type']}");
-        }
-      } else if (question is String) {
-        // Handle simple text questions
-        questionWidgets.add(
-          ListTile(
-            title: Text(key),
-            subtitle: Text(question),
-          ),
-        );
-      } else{
-        print("Invalid question format for: $key -> $question");
-      }
+      // Asumsi semua pertanyaan adalah teks karena tidak ada data lain
+      questionWidgets.add(ListTile(
+        title: Text(key),
+        subtitle: Text(question), // 'question' sebenarnya adalah jawaban
+      ));
     });
+
     return questionWidgets;
   }
 
+
+  // List<Widget> _buildQuestions(Map<String, dynamic> questions) {
+  //   List<Widget> questionWidgets = [];
+  //   print("Building questions from: $questions");
+  //
+  //   questions.forEach((key, question) {
+  //     switch (question['type']) {
+  //       case 'text':
+  //         questionWidgets.add(_buildTextQuestion(key, question['prompt']));
+  //         break;
+  //       case 'dropdown':
+  //         questionWidgets.add(_buildDropdownQuestion(key, question['prompt']));
+  //         break;
+  //       case 'checkbox':
+  //         questionWidgets.add(_buildCheckboxQuestion(key, question['prompt']));
+  //         break;
+  //       default:
+  //         print("Unknown question type: ${question['type']}");
+  //     }
+  //   });
+  //   return questionWidgets;
+  // }
 
   // Existing methods for dropdown questions
   Widget _buildDropdownQuestion(String key, Map<String, dynamic> question) {
@@ -182,8 +207,44 @@ class _FormDetailState extends State<FormDetail> {
     );
   }
 
+  // Existing methods for checklist questions
+  Widget _buildCheckboxQuestion(String key, Map<String, dynamic> question) {
+    if (!(question['options'] is List)) {
+      print("Error: Options for $key are not in list format: ${question['options']}");
+      return Text("Error: invalid options format");
+    }
+    List<String> options = List<String>.from(question['options']);
+    List<String> currentAnswers = List<String>.from(question['answer'] ?? []);
+
+    return ListTile(
+      title: Text(question['prompt'] ?? 'Question'),
+      subtitle: Column(
+        children: options.map((option) {
+          return CheckboxListTile(
+            title: Text(option),
+            value: currentAnswers.contains(option),
+            onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                  if (!currentAnswers.contains(option)) {
+                    currentAnswers.add(option);
+                  }
+                } else {
+                  currentAnswers.remove(option);
+                }
+                _formData![key]['answer'] = currentAnswers;
+              });
+            },
+            controlAffinity: ListTileControlAffinity.leading, // Lokasi checkbox
+          );
+        }).toList(),
+      ),
+    );
+  }
+
   // Existing methods for multiple-choice questions
-  Widget _buildMultipleChoiceQuestion(String key, Map<String, dynamic> question) {
+  Widget _buildMultipleChoiceQuestion(
+      String key, Map<String, dynamic> question) {
     List<String> selectedOption = List<String>.from(question['answer'] ?? []);
     List<dynamic> options = question['options'] ?? [];
 
@@ -194,10 +255,10 @@ class _FormDetailState extends State<FormDetail> {
           return CheckboxListTile(
             value: selectedOption.contains(option),
             onChanged: (bool? value) {
-              setState((){
-                if (value == true){
+              setState(() {
+                if (value == true) {
                   selectedOption.add(option as String);
-                }else {
+                } else {
                   selectedOption.remove(option);
                 }
                 _formData![key]['answer'] = selectedOption;
@@ -212,12 +273,13 @@ class _FormDetailState extends State<FormDetail> {
 
   // Existing methods for text input questions
   Widget _buildTextQuestion(String key, Map<String, dynamic> question) {
-    TextEditingController controller = TextEditingController(text: question['answer']?.toString() ?? '');
+    TextEditingController controller =
+        TextEditingController(text: question['answer']?.toString() ?? '');
     return ListTile(
       title: Text(question['question'] ?? ''),
       subtitle: TextField(
         controller: controller,
-        onChanged: (value){
+        onChanged: (value) {
           _formData![key]['answer'] = value;
         },
         decoration: const InputDecoration(
