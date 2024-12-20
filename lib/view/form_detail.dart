89d79e21eb900879604b83b194081c693bc3b5e8
sachikoyaa <feliciajacintha@gmail.com>
2024-12-20 14:59:16 +0700

    push dulu

--- lib/view/form_detail.dart
+++ lib/view/form_detail.dart
@@ -1,35 +1,18 @@
-import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:drone_checklist/model/checklist_form_model.dart';
-import 'package:drone_checklist/Database/database_helper.dart';
-import 'checklist_form_view.dart';
+import 'package:drone_checklist/database/database_helper.dart';
+import 'package:flutter/material.dart';
 
 class FormDetail extends StatefulWidget {
   final int formId;
-  final String formName;
-  final int templateId;
 
-  const FormDetail({
-    Key? key,
-    required this.formId,
-    required this.formName,
-    required this.templateId,
-  }) : super(key: key);
+  const FormDetail({Key? key, required this.formId}) : super(key: key);
 
   @override
   _FormDetailState createState() => _FormDetailState();
 }
 
 class _FormDetailState extends State<FormDetail> {
-  final _formKey = GlobalKey<FormState>();
-  final Map<String, TextEditingController> _questionControllers = {};
-  final Map<String, String> _dropdownValues = {};
-  final Map<String, String> _multipleValues = {};
-  final Map<String, String> _textboxValues = {};
-
   Map<String, dynamic>? _formData;
-  Map<String, dynamic>? _templateData;
-
   String? _formName;
   bool _isLoading = true;
 
@@ -39,88 +22,91 @@ class _FormDetailState extends State<FormDetail> {
     _loadFormData(widget.formId);
   }
 
-  void _updateForm() {
-    if(_formKey.currentState?.validate() ?? false){
-      Map<String, dynamic> formData = {};
-      Map<String, dynamic> templateData = {};
-
-      _questionControllers.forEach((key, controller){
-        formData[key] = _questionControllers.values;
-      });
-
-      formData.addAll(_textboxValues);
-      formData.addAll(_multipleValues);
-      formData.addAll(_dropdownValues);
-
-      final formModel = ChecklistFormModel(
-        formId: widget.formId, //take the chosen formId
-        templateId: widget.templateId, //take the form's templateId
-        formName: widget.formName, //take the form's name
-        formData: formData, //the form's data
-        templateData: templateData, // get the template's data
-        updatedDate: DateTime.now(), //the template's data
-      )
-    }
+  void _saveForm() {
+    // Handle form saving logic here
     print("Form saved!");
   }
 
   Future<void> _loadFormData(int formId) async {
     try {
       var form = await DatabaseHelper.getFormById(formId);
-      //debug form data
-      //print("Form data: $form");
-
       if (form == null) {
-        setState(() {
-          _isLoading = false;
-        });
-        print("Form data not found!");
+        setState(() => _isLoading = false);
+        print("Form data not found!: $formId");
         return;
       }
 
-      var template = await DatabaseHelper.getTemplateById(form['templateId']);
+      print("Form data retrieved: $form");
 
-      if (template == null) {
-        setState(() {
-          _isLoading = false;
-        });
-        print("Template data not found!");
-        return;
-      }
-
-      //debug template data
-      //print("Template data: ${template['templateFormData']}");
-
-      Map<String, dynamic> templateData = Map<String, dynamic>.from(jsonDecode(template['templateFormData']));
-      Map<String, dynamic> formData = form['formData'] != null
-          ? Map<String, dynamic>.from(jsonDecode(form['formData']))
-          : {};
-
-      Map<String, dynamic> displayedForm = {};
-      templateData['questions'].forEach((key, question){
-        displayedForm[key] = {
-          ...question,
-          'answer': formData[key] ?? '',
-        };
-      });
-
+      var formData =
+      form['formData'] != null ? jsonDecode(form['formData']) : {};
+      print("Decoded form data: $formData");
       setState(() {
-        // Decode the JSON stored in the formData column
         _formName = form['formName'];
-        //debug merged form
-        //print("Merged Form: $displayedForm");
-        _formData = displayedForm;
-        _templateData = templateData;
+        _formData = formData;
         _isLoading = false;
       });
-
-      } catch (e) {
-      setState(() {
-        _isLoading = false;
-      });
-      print("Error loading form or template data: $e");
+    } catch (e) {
+      setState(() => _isLoading = false);
+      print("Error loading form data: $e");
     }
   }
+
+  // Future<void> _loadFormData(int formId) async {
+  //   try {
+  //     var form = await DatabaseHelper.getFormById(formId);
+  //     //debug form data
+  //     //print("Form data: $form");
+  //
+  //     if (form == null) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       print("Form data not found!");
+  //       return;
+  //     }
+  //
+  //     var template = await DatabaseHelper.getTemplateById(form['templateId']);
+  //
+  //     if (template == null) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //       print("Template data not found!");
+  //       return;
+  //     }
+  //
+  //     //debug template data
+  //     //print("Template data: ${template['templateFormData']}");
+  //
+  //     Map<String, dynamic> templateData =
+  //         jsonDecode(template['templateFormData']);
+  //     Map<String, dynamic> formData =
+  //         form['formData'] != null ? jsonDecode(form['formData']) : {};
+  //
+  //     Map<String, dynamic> displayedForm = {};
+  //     templateData['questions'].forEach((key, question) {
+  //       displayedForm[key] = {
+  //         ...question,
+  //         'answer': formData[key] ?? '',
+  //       };
+  //     });
+  //
+  //     setState(() {
+  //       // Decode the JSON stored in the formData column
+  //       _formName = form['formName'];
+  //       //debug merged form
+  //       //print("Merged Form: $displayedForm");
+  //       _formData = displayedForm;
+  //       _isLoading = false;
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //     print("Error loading form or template data: $e");
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -131,81 +117,58 @@ class _FormDetailState extends State<FormDetail> {
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : _formData == null
-              ? const Center(child: Text('No form data available'))
-              : Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ListView(
-                    children: [
-                      Text(
-                        _formName ?? 'Untitled Form',
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ..._buildQuestions(_formData!),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: _saveForm,
-                        child: const Text('Save Changes'),
-                      ),
-                    ],
+          ? const Center(child: Text('No form data available'))
+          : Builder(
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
+              children: [
+                Text(
+                  _formName ?? 'Untitled Form',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
-          )
+                ),
+                const SizedBox(height: 20),
+                ..._buildQuestions(_formData!),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _saveForm,
+                  child: const Text('Save Changes'),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 
-  // Use your existing method to build questions
   List<Widget> _buildQuestions(Map<String, dynamic> questions) {
-    //print("Building questions: $questions");
     List<Widget> questionWidgets = [];
+    print("Building questions from: $questions");
 
     questions.forEach((key, question) {
-      //debug form field
-      //print("Processing: $key -> $question");
-
-      if (question is Map) {
-        final questionMap = Map<String, dynamic>.from(question);
-
-        switch (questionMap['type']) {
-          case 'dropdown':
-            questionWidgets.add(_buildDropdownQuestion(key, questionMap));
-            break;
-          case 'multiple':
-            questionWidgets.add(_buildMultipleChoiceQuestion(key, questionMap));
-            break;
-          case 'text':
-            questionWidgets.add(_buildTextQuestion(key, questionMap));
-            break;
-          default:
-            print("Unknown question type: ${questionMap['type']}");
-        }
-      // } else if (question is String) {
-      //   // Handle simple text questions
-      //   questionWidgets.add(
-      //     ListTile(
-      //       title: Text(key),
-      //       subtitle: Text(question),
-      //     ),
-      //   );
-      // } else{
-      //   print("Invalid question format for: $key -> $question");
-      }
+      // Asumsi semua pertanyaan adalah teks karena tidak ada data lain
+      questionWidgets.add(ListTile(
+        title: Text(key.toString()),
+        subtitle: Text(question.toString()), // 'question' sebenarnya adalah jawaban
+      ));
     });
+
     return questionWidgets;
   }
 
-
   // Existing methods for dropdown questions
   Widget _buildDropdownQuestion(String key, Map<String, dynamic> question) {
-    List<dynamic> options = question['options'] is List ? question['options'] : [];
-
     return ListTile(
       title: Text(question['question'] ?? 'Question'),
       subtitle: DropdownButton<String>(
         value: question['answer'] != '' ? question['answer'] : null,
-        items: options.map<DropdownMenuItem<String>>((option) {
+        items: (question['options'] as List<dynamic>?)
+            ?.map<DropdownMenuItem<String>>((option) {
           return DropdownMenuItem<String>(
             value: option as String?,
             child: Text(option.toString()),
@@ -221,10 +184,46 @@ class _FormDetailState extends State<FormDetail> {
     );
   }
 
+  // Existing methods for checklist questions
+  Widget _buildCheckboxQuestion(String key, Map<String, dynamic> question) {
+    if (!(question['options'] is List)) {
+      print("Error: Options for $key are not in list format: ${question['options']}");
+      return Text("Error: invalid options format");
+    }
+    List<String> options = List<String>.from(question['options']);
+    List<String> currentAnswers = List<String>.from(question['answer'] ?? []);
+
+    return ListTile(
+      title: Text(question['prompt'] ?? 'Question'),
+      subtitle: Column(
+        children: options.map((option) {
+          return CheckboxListTile(
+            title: Text(option),
+            value: currentAnswers.contains(option),
+            onChanged: (bool? value) {
+              setState(() {
+                if (value == true) {
+                  if (!currentAnswers.contains(option)) {
+                    currentAnswers.add(option);
+                  }
+                } else {
+                  currentAnswers.remove(option);
+                }
+                _formData![key]['answer'] = currentAnswers;
+              });
+            },
+            controlAffinity: ListTileControlAffinity.leading, // Lokasi checkbox
+          );
+        }).toList(),
+      ),
+    );
+  }
+
   // Existing methods for multiple-choice questions
-  Widget _buildMultipleChoiceQuestion(String key, Map<String, dynamic> question) {
+  Widget _buildMultipleChoiceQuestion(
+      String key, Map<String, dynamic> question) {
     List<String> selectedOption = List<String>.from(question['answer'] ?? []);
-    List<dynamic> options = question['options'] is List ? question['options'] : [];
+    List<dynamic> options = question['options'] ?? [];
 
     return ListTile(
       title: Text(question['question'] ?? 'Question'),
@@ -233,10 +232,10 @@ class _FormDetailState extends State<FormDetail> {
           return CheckboxListTile(
             value: selectedOption.contains(option),
             onChanged: (bool? value) {
-              setState((){
-                if (value == true){
+              setState(() {
+                if (value == true) {
                   selectedOption.add(option as String);
-                }else {
+                } else {
                   selectedOption.remove(option);
                 }
                 _formData![key]['answer'] = selectedOption;
@@ -251,12 +250,13 @@ class _FormDetailState extends State<FormDetail> {
 
   // Existing methods for text input questions
   Widget _buildTextQuestion(String key, Map<String, dynamic> question) {
-    TextEditingController controller = TextEditingController(text: question['answer']?.toString() ?? '');
+    TextEditingController controller =
+    TextEditingController(text: question['answer']?.toString() ?? '');
     return ListTile(
       title: Text(question['question'] ?? ''),
       subtitle: TextField(
         controller: controller,
-        onChanged: (value){
+        onChanged: (value) {
           _formData![key]['answer'] = value;
         },
         decoration: const InputDecoration(
--- lib/view/form_view.dart
+++ lib/view/form_view.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
-import 'package:dio/dio.dart';
 import 'package:drone_checklist/database/database_helper.dart';
-import 'package:drone_checklist/services/api_service.dart';
 import 'package:drone_checklist/view/form_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:drone_checklist/view/template_view.dart';
@@ -78,51 +74,6 @@ class _FormCreateState extends State<FormCreate> {
 
     //debug ambil id yang dipilih
     print("Syncing ID(s): $selectedForms");
-    
-    if(selectedForms.isEmpty){
-      ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('No forms selected to sync!')),
-      );
-      return;
-    }
-    
-    try{
-      List<Map<String, dynamic>?> formToSync = await Future.wait(
-        selectedForms.map((formId) => DatabaseHelper.getFormById(formId)),
-      );
-
-      List<Map<String, dynamic>?> syncData = formToSync.map((form){
-        return {
-          "submissionName": form?['formName'],
-          "templateId": form?['templateId'],
-          "submittedBy": "User", //hardcode dulu ya
-          "submittedDate": DateTime.now().toIso8601String(),
-        "formData": form?['formData'],
-        };
-      }).toList();
-
-      //panggil apiapi
-      final dio = Dio();
-      final apiService = ApiService(dio);
-
-      List<Map<String, dynamic>> newSyncData = syncData.where((map) => map != null).cast<Map<String, dynamic>>().toList();
-
-      //sync process
-      for(var data in newSyncData){
-        print(const JsonEncoder.withIndent('  ').convert(data));
-        final response = await apiService.syncData(data);
-        print("Sync Succesful: $response");
-      }
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Syncing Succesful!')),
-      );
-    } catch (e){
-      print("Sync Error: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Sync Error: $e'))
-      );
-    }
   }
 
   @override
@@ -160,64 +111,64 @@ class _FormCreateState extends State<FormCreate> {
       ]),
       body: _formList.isEmpty
           ? const Center(
-              child: Text(
-                "No Form Available Yet :(",
-                style: TextStyle(fontSize: 18),
-              ),
-            )
+        child: Text(
+          "No Form Available Yet :(",
+          style: TextStyle(fontSize: 18),
+        ),
+      )
           : ListView.builder(
-              itemCount: _formList.length,
-              itemBuilder: (context, index) => Card(
-                margin: const EdgeInsets.all(15),
-                child: ListTile(
-                  leading:const Icon(Icons.description, color: Colors.blue),
-                  title: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      _formList[index]['formName'],
-                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => FormDetail(
-                        formId: _formList[index]
-                            ['formId'], // Pass the selected form ID
-                      ),
-                    ),
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          int formId = _formList[index]['formId'];
-                          await DatabaseHelper.deleteForm(formId);
-                          setState(() {
-                            _formList.removeAt(index);
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.delete,
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      Checkbox(
-                        value: _formList[index]['isChecked'] ?? false,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _formList[index]['isChecked'] = value ?? false;
-                          });
-                          //debug changestate
-                          // print('Checkbox formId: ${_formList[index]['formId']} set to $value');
-                        },
-                      ),
-                    ],
-                  ),
+        itemCount: _formList.length,
+        itemBuilder: (context, index) => Card(
+          margin: const EdgeInsets.all(15),
+          child: ListTile(
+            leading:const Icon(Icons.description, color: Colors.blue),
+            title: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Text(
+                _formList[index]['formName'],
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FormDetail(
+                  formId: _formList[index]
+                  ['formId'], // Pass the selected form ID
                 ),
               ),
             ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  onPressed: () async {
+                    int formId = _formList[index]['formId'];
+                    await DatabaseHelper.deleteForm(formId);
+                    setState(() {
+                      _formList.removeAt(index);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                Checkbox(
+                  value: _formList[index]['isChecked'] ?? false,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _formList[index]['isChecked'] = value ?? false;
+                    });
+                    //debug changestate
+                    // print('Checkbox formId: ${_formList[index]['formId']} set to $value');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
